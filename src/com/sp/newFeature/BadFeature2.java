1c4ed38ec2749b7f364ed740b2cac11948d63e24
wujiefeng3mo <wujiefeng3mo@outlook.com>
2017-07-18 22:56:40 -0400

    First Try!@

---
+++ src/com/sp/newFeature/BadFeature2.java
@@ -0,0 +1,11 @@
+package com.sp.newFeature;
+
+public class BadFeature2 {
+	
+	
+	public void Test() {
+		System.out.println("fdsafds");
+	}
+	
+	
+}
---
+++ src/com/sp/newFeature/BadFeature3.java
@@ -0,0 +1,11 @@
+package com.sp.newFeature;
+
+public class BadFeature3 {
+	
+	
+	public void Test() {
+		System.out.println("fdsafds");
+	}
+	
+	
+}
